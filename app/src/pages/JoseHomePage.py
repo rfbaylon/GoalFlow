258b038fed46ee26da65f09940324d726aa774aa
RishikKelkar <rishikk871@gmail.com>
2025-08-12 22:01:52 -0400

    l

--- app/src/pages/JoseHomePage.py
+++ app/src/pages/JoseHomePage.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from modules.nav import SideBarLinks
+
 
 st.set_page_config(layout='wide')
 
