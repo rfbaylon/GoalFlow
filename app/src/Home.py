9afde434026f8cbeb2648af7e3ffcd6437e08de5
Jaden Hu <jadenhu2005@gmail.com>
2025-08-13 20:49:39 -0400

    removed goal a goal b goal c

--- app/src/Home.py
+++ app/src/Home.py
@@ -20,37 +20,7 @@ left, right = st.columns(2)
 #2 - due
 #3 -desc
 
-with left:
-    a, b, c = st.columns(3)
-    with a:
-        st.header("Goal A", divider=True)
-        st.subheader(active[0][1]) # Goal name
-        st.write(f"Due :red[{active[0][2]}]") # Due date
-        st.write(active[0][3]) # Description
-        st.write("### Subgoals") # Subgoals
-        for subgoal in subgoals:
-            if subgoal[0] == active[0][0]:
-                st.write(f"- {subgoal[1]}") 
 
-    with b:
-        st.header("Goal B", divider=True)
-        st.subheader(active[1][1])
-        st.write(f"Due :red[{active[1][2]}]")
-        st.write(active[1][3])
-        st.write("### Subgoals")
-        for subgoal in subgoals:
-            if subgoal[0] == active[1][0]:
-                st.write(f"- {subgoal[1]}")        
-
-    with c:
-        st.header("Goal C", divider=True)
-        st.subheader(active[2][1])
-        st.write(f"Due :red[{active[2][2]}]")
-        st.write(active[2][3])
-        st.write("### Subgoals")
-        for subgoal in subgoals:
-            if subgoal[0] == active[2][0]:
-                st.write(f"- {subgoal[1]}")  
 
 with right:
     st.header("Right Half")
